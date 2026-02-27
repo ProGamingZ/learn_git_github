7894a4710a1291cfe03e642c36d99ae888ae34fc
YJAO <ortizyuri254@gmail.com>
2026-02-27 23:31:26 +0800

    playing with functions

--- greet.js
+++ greet.js
@@ -1,6 +1,21 @@
 console.log("Hello, World!");
 console.log("Hello");
 
+// sample function expression
+const multiply = function (a, b) {return a * b};
+console.log(multiply(2, 3));
+
+
+// sample func arrows
+const func0 = () => {"Hello World!"};
+console.log(func0());
+
+const func1 = (val) => {"Hello World!" + val};
+console.log(func1(" from JavaScript penultimate!"));
+
+const func3 = val => "final hello!" + val;
+console.log(func3(" from JavaScript last!"));
+
 // THIS WILL GO TO STASH
 // CODE
 // CODE
