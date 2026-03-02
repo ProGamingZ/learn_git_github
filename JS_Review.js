e31bb9300e171ed7045c6781fb3e60ef6da2bcce
YJAO <ortizyuri254@gmail.com>
2026-03-02 21:38:33 +0800

    add review section #2

--- JS_Review.js
+++ JS_Review.js
@@ -1,4 +1,4 @@
-// Arrow Functions vs. Regular Functions 
+// Arrow Functions vs. Regular Functions #1
 function sample()
 {
    // console.log("Hello, World!");
@@ -7,18 +7,47 @@ function sample()
 
 let x = sample();
 let y = () => { return " World!"};
-console.log(x + y());
+// console.log(x + y());
 
 // sample function expression
 const multiply = function (a, b) {return a * b};
-console.log(multiply(2, 3));
+// console.log(multiply(2, 3));
 
 // sample func arrows
 const func0 = () => {return "Hello World!"};
-console.log(func0());
+// console.log(func0());
 
 const func1 = (val) => { return "Hello World!" + val};
-console.log(func1(" from JavaScript penultimate!"));
+// console.log(func1(" from JavaScript penultimate!"));
 
 const func2 = val => "final hello!" + val;
-console.log(func2(" from JavaScript last!"));
+// console.log(func2(" from JavaScript last!"));
+
+
+// Destructuring (Objects and Arrays) for cleaner props and state #2
+const person = { firstName: "John", lastName: "Doe", age: 30 }; 
+const { firstName, lastName } = person;
+console.log(firstName); // Output: John
+console.log(lastName);  // Output: Doe
+
+const Name = "John Doe";
+const [a1, ...a6] = Name;
+console.log(a1);
+console.log(a6);
+
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+for (const [fruit, quantity] of fruits) {
+  console.log(`${fruit}: ${quantity}`);
+}
+
+const {0:b0,...b1} = [...fruits];
+console.log(b0);
+console.log(b1);
+
+const [b2,...b3] = [...fruits];
+console.log(b2);
+console.log(b3); 
