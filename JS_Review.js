f4df7c6eabcb81cfa4328a651fc9e44c941463d5
YJAO <ortizyuri254@gmail.com>
2026-03-02 21:10:44 +0800

    cleaned greet.js, added review section #1 to JS_Review.js

--- JS_Review.js
+++ JS_Review.js
@@ -1,11 +1,24 @@
+// Arrow Functions vs. Regular Functions 
 function sample()
 {
    // console.log("Hello, World!");
    return "Hello";
 }
 
-
-
 let x = sample();
 let y = () => { return " World!"};
 console.log(x + y());
+
+// sample function expression
+const multiply = function (a, b) {return a * b};
+console.log(multiply(2, 3));
+
+// sample func arrows
+const func0 = () => {return "Hello World!"};
+console.log(func0());
+
+const func1 = (val) => { return "Hello World!" + val};
+console.log(func1(" from JavaScript penultimate!"));
+
+const func2 = val => "final hello!" + val;
+console.log(func2(" from JavaScript last!"));
--- greet.js
+++ greet.js
@@ -1,21 +1,6 @@
 console.log("Hello, World!");
 console.log("Hello");
 
-// sample function expression
-const multiply = function (a, b) {return a * b};
-console.log(multiply(2, 3));
-
-
-// sample func arrows
-const func0 = () => {return "Hello World!"};
-console.log(func0());
-
-const func1 = (val) => { return "Hello World!" + val};
-console.log(func1(" from JavaScript penultimate!"));
-
-const func3 = val => "final hello!" + val;
-console.log(func3(" from JavaScript last!"));
-
 // THIS WILL GO TO STASH
 // CODE
 // CODE
